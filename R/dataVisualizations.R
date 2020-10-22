9b7fc163a4f98cfd165431662b9a9d1064e86c5c
Gillian McGinnis <gmcginnis@rstudio.reed.edu>
2020-10-22 10:25:21 -0700

    More data viz tests

--- R/dataVisualizations.R
+++ R/dataVisualizations.R
@@ -3,9 +3,13 @@
 ## Created 21 October 2020
 ## Updated 21 October 2020
 
+library(ggthemes)
 source("R/dataWranglingNeat.R") ## Runs necessary warngles and prepares environment
 rm(list = setdiff(ls(), c("allData", "dataClinton_final", "dataWoodstock_final"))) ## Cleans up environment; keep only dataframes of use
 
+## Adjusting labels for later
+pmOrder <- c("pm01", "pm25", "pm10")
+pmLabels <- c("PM 1.0", "PM 2.5", "PM 10")
 
 ## Data viz tests!
 ## Note: loess method for geom_smooth takes time to process. "gam" method is faster but gives much curvier lines.
@@ -16,7 +20,12 @@ ggplot(allData, aes(x = hmsTime,
                     color = pmType))+
   geom_point(alpha = 0.3)+
   geom_smooth(method = "loess")+
-  facet_wrap(~set)
+  facet_wrap(~set)+
+  scale_color_discrete(breaks = pmOrder,
+                       labels = pmLabels)+
+  labs(color = "PM Type",
+       x = "Time",
+       y = "PM Value (ppm)")
 
 ## 6 scatterplots; facets by location, does not include set (i.e. Woodstock or Clinton) (but this is implied by location)
 ggplot(allData, aes(x = hmsTime,
@@ -32,3 +41,32 @@ ggplot(allData, aes(x = hmsTime,
   geom_point(alpha = 0.3)+
   geom_smooth(method = "loess")+
   facet_grid(rows = vars(location), cols = vars(pmType))
+
+## 6 scatterplots; facets by location, color by PM type
+ggplot(allData, aes(x = hmsTime,
+                    y = pmValues,
+                    color = pmType))+
+  geom_point(alpha = 0.3)+
+  geom_smooth(method = "loess")+
+  facet_wrap(~location)
+
+## 2 boxplots
+ggplot(allData, aes(x = location,
+                    y = pmValues,
+                    color = pmType))+
+  facet_wrap(~set, scales = "free")+
+  geom_boxplot()
+
+ggplot(allData, aes(x = location,
+                    y = pmValues,
+                    color = pmType))+
+  facet_grid(.~set, scales = "free", space = "free")+
+  geom_boxplot(color = "gray50", outlier.shape = NA, fill = NA)+
+  geom_point(alpha = 0.7, position = position_jitterdodge())
+
+ggplot(allData, aes(x = hmsTime,
+                    y = pmValues,
+                    color = pmType))+
+  geom_point(alpha = 0.3)+
+  geom_smooth(method = "loess")+
+  facet_wrap(~location)
